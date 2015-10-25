645eb24f540fae80dc5ad43eee9d9643eda43c74
kevintownsend <kt@krtownsend.com>
2015-10-25 16:29:46 -0500

    .

--- dual_port_xor_ram.v
+++ dual_port_xor_ram.v
@@ -1,7 +1,8 @@
-module dual_port_xor_ram(clk, wr_a, addr_a, q_a, wr_b, addr_b, q_b);
+module dual_port_xor_ram(clk, rst, wr_a, addr_a, q_a, wr_b, addr_b, q_b);
 parameter DEPTH = 512;
 parameter ADDR_WIDTH = log2(DEPTH - 1);
 input clk;
+input rst;
 input wr_a;
 input [ADDR_WIDTH - 1:0] addr_a;
 output q_a;
@@ -25,16 +26,42 @@ wire ram_01_q = ram_0[addr_b];
 wire ram_10_q = ram_1[addr_a];
 wire ram_11_q = ram_1[addr_b];
 
-assign q_a = ram_00_q ^ ram_01_q;
-assign q_b = ram_10_q ^ ram_11_q;
+assign q_a = ram_00_q ^ ram_10_q;
+assign q_b = ram_01_q ^ ram_11_q;
 
-wire tmp0 = ram_00_q ^ wr_a;
+always @(posedge clk) begin
+    if(wr_a) begin
+        $display("ram_0[0]: %H", ram_0[0]);
+        $display("ram_1[0]: %H", ram_1[0]);
+    end
+    if(wr_a) begin
+        $display("ram_0[1]: %H", ram_0[1]);
+        $display("ram_1[1]: %H", ram_1[1]);
+    end
+    if(wr_b != 0) begin
+        $display("wr_b: %d", wr_b);
+    end
+end
+wire tmp0 = rst ? 0 : ram_00_q ^ wr_a;
 always @(posedge clk) begin
     ram_0[addr_a] <= tmp0;
 end
-wire tmp1 = ram_11_q ^ wr_b;
+wire tmp1 = rst ? 0 : ram_11_q ^ wr_b;
 always @(posedge clk) begin
     ram_1[addr_b] <= tmp1;
 end
+/*
+always @(posedge clk) begin
+    $display();
+    $display("xor debug %d", $time);
+    $display("rst: %d", rst);
+    $display("ram_1[0]: %d", ram_1[0]);
+    $display("ram_11_q: %d", ram_11_q);
+    $display("tmp1: %d", tmp1);
+    $display("wr_b: %d", wr_b);
+    $display("end xor debug");
+    $display();
+end
+*/
 `include "common.vh"
 endmodule
