2acfed336ed6ff478061b10323fd84779043e0b2
Kevin Townsend <kt@krtownsend.com>
2015-11-06 13:58:29 -0600

    .

--- dual_port_xor_ram.v
+++ dual_port_xor_ram.v
@@ -29,19 +29,6 @@ wire ram_11_q = ram_1[addr_b];
 assign q_a = ram_00_q ^ ram_10_q;
 assign q_b = ram_01_q ^ ram_11_q;
 
-always @(posedge clk) begin
-    if(wr_a) begin
-        $display("ram_0[0]: %H", ram_0[0]);
-        $display("ram_1[0]: %H", ram_1[0]);
-    end
-    if(wr_a) begin
-        $display("ram_0[1]: %H", ram_0[1]);
-        $display("ram_1[1]: %H", ram_1[1]);
-    end
-    if(wr_b != 0) begin
-        $display("wr_b: %d", wr_b);
-    end
-end
 wire tmp0 = rst ? 0 : ram_00_q ^ wr_a;
 always @(posedge clk) begin
     ram_0[addr_a] <= tmp0;
@@ -50,18 +37,5 @@ wire tmp1 = rst ? 0 : ram_11_q ^ wr_b;
 always @(posedge clk) begin
     ram_1[addr_b] <= tmp1;
 end
-/*
-always @(posedge clk) begin
-    $display();
-    $display("xor debug %d", $time);
-    $display("rst: %d", rst);
-    $display("ram_1[0]: %d", ram_1[0]);
-    $display("ram_11_q: %d", ram_11_q);
-    $display("tmp1: %d", tmp1);
-    $display("wr_b: %d", wr_b);
-    $display("end xor debug");
-    $display();
-end
-*/
 `include "common.vh"
 endmodule
